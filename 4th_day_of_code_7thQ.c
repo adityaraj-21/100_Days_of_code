51156af3def4e8c7541c43cc8b6dc67dbe3e0322
adityaraj-21 <adityaraj.ar21g7@gmail.com>
2025-11-24 19:52:37 +0530

    Add files via upload

---
+++ 4th_day_of_code_7thQ.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+
+    printf("Enter two numbers to swap: ");
+    scanf("%d %d", &a, &b);
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("\nAfter swap: %d %d\n", a, b);
+
+    return 0;
+}
